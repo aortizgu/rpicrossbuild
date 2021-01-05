d83d24e770a197b79896e4b37423a94cc308fea1
Adrián Ortiz Gutiérrez <ortiz.gutierrez.adrian@gmail.com>
2021-01-05 20:10:51 +0100

    add sysroot folder

--- rpi2-32/update_sysroot.sh
+++ rpi2-32/update_sysroot.sh
@@ -2,6 +2,7 @@
 mkdir sysroot
 rsync -rvzLR --safe-links \
       ubuntu@$1:/usr/lib/arm-linux-gnueabihf \
+      ubuntu@$1:/usr/lib/gcc/arm-linux-gnueabihf \
       ubuntu@$1:/usr/include \
       ubuntu@$1:/lib/arm-linux-gnueabihf \
       sysroot/
